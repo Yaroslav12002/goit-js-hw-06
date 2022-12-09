a559f434dc9bacdc49db817f1c46eddf83b9410e
Yaroslav12002 <my.mail12002@gmail.com>
2022-12-09 22:40:41 +0200

    task 9 ready

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColorElement = document.querySelector('button.change-color');
+const bodyElement = document.querySelector('body');
+const spanElement = document.querySelector('span.color');
+
+buttonChangeColorElement.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  const newColor = getRandomHexColor();
+  bodyElement.setAttribute('style', `background-color: ${newColor}`);
+  spanElement.textContent = newColor;
+}
+
+// Задание 9
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн
+// стиль при клике на button.change-color и выводит значение цвета в span.color.
